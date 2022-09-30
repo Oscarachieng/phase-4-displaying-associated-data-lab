2d9fca771dc384317df059b76f278be40cfc5267
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-09-30 11:41:21 +0300

    commit tree e21c2f5e9e7d6b1e0e1bc4a098d986f60a679fec

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+    #GET /index
+    def index
+        items = Item.all.order(price: :desc)
+        render json: items, include: :user, status: :ok
+    end
 end
